29fcb012cda18212f27ee06aa35181655e9cb692
Maxence Frenette <maxence.frenette@gmail.com>
2024-03-04 00:59:23 -0800

    Remove unused import

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -18,7 +18,6 @@ import {
   TableHeader,
   TableRow,
 } from "~/components/ui/table";
-import { cn } from "~/lib/utils";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
