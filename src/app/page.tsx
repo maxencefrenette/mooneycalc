8a467912f5218365cfb05ec75d5e07040e2ed473
Maxence Frenette <maxence.frenette@gmail.com>
2024-03-03 22:21:09 -0800

    Move back column definitions outside of component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,48 +4,48 @@ import { type ActionDetail, getActions } from "~/services/actions";
 import { type ColumnDef, createColumnHelper } from "@tanstack/react-table";
 import { DataTable } from "~/components/data-table";
 
-export default function HomePage() {
-  const columnHelper = createColumnHelper<ActionDetail>();
+const columnHelper = createColumnHelper<ActionDetail>();
 
-  const columns: ColumnDef<ActionDetail>[] = [
-    {
-      accessorKey: "name",
-      header: "Name",
+const columns: ColumnDef<ActionDetail>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  columnHelper.display({
+    id: "inputs",
+    header: "Inputs",
+    cell: ({ row }) => {
+      const actionDetails = row.original;
+      return (
+        <>
+          {actionDetails.inputItems?.map((input) => (
+            <div key={input.itemHrid}>
+              {input.count} x {input.itemHrid}
+            </div>
+          ))}
+        </>
+      );
     },
-    columnHelper.display({
-      id: "inputs",
-      header: "Inputs",
-      cell: ({ row }) => {
-        const actionDetails = row.original;
-        return (
-          <>
-            {actionDetails.inputItems?.map((input) => (
-              <div key={input.itemHrid}>
-                {input.count} x {input.itemHrid}
-              </div>
-            ))}
-          </>
-        );
-      },
-    }),
-    columnHelper.display({
-      id: "outputs",
-      header: "Outputs",
-      cell: ({ row }) => {
-        const actionDetails = row.original;
-        return (
-          <>
-            {actionDetails.outputItems?.map((output) => (
-              <div key={output.itemHrid}>
-                {output.count} x {output.itemHrid}
-              </div>
-            ))}
-          </>
-        );
-      },
-    }),
-  ];
+  }),
+  columnHelper.display({
+    id: "outputs",
+    header: "Outputs",
+    cell: ({ row }) => {
+      const actionDetails = row.original;
+      return (
+        <>
+          {actionDetails.outputItems?.map((output) => (
+            <div key={output.itemHrid}>
+              {output.count} x {output.itemHrid}
+            </div>
+          ))}
+        </>
+      );
+    },
+  }),
+];
 
+export default function HomePage() {
   const actions = getActions();
 
   return (
