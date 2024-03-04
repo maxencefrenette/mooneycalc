97037a39d2a47cce06890ca0a87c0f7cb52106eb
Maxence Frenette <maxence.frenette@gmail.com>
2024-03-03 23:06:23 -0800

    Add proper player stats form UI

--- src/components/player-stats-form.tsx
+++ src/components/player-stats-form.tsx
@@ -1,4 +1,8 @@
 import { type PlayerStats } from "~/services/player-stats";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { gameData } from "~/services/data";
+import { sortedSkills } from "~/services/skills";
 
 export interface PlayerStatsFormProps {
   playerStats: PlayerStats;
@@ -11,39 +15,34 @@ export function PlayerStatsForm({
 }: PlayerStatsFormProps) {
   return (
     <div>
-      <h1>Player Stats</h1>
-      <div>
-        {Object.entries(playerStats.levels).map(([skillHrid, level]) => (
-          <div key={skillHrid}>
-            {skillHrid}: {level}
-            <button
-              onClick={() =>
-                updatePlayerStats({
-                  ...playerStats,
-                  levels: {
-                    ...playerStats.levels,
-                    [skillHrid]: level + 1,
-                  },
-                })
-              }
+      <h1 className="pb-4 text-xl">Player Stats</h1>
+      <div className="flex flex-wrap gap-4">
+        {sortedSkills.map(({ hrid, name }) => {
+          const level = playerStats.levels[hrid]!;
+
+          return (
+            <div
+              key={hrid}
+              className="grid w-full max-w-xs items-center gap-1.5"
             >
-              +
-            </button>
-            <button
-              onClick={() =>
-                updatePlayerStats({
-                  ...playerStats,
-                  levels: {
-                    ...playerStats.levels,
-                    [skillHrid]: level - 1,
-                  },
-                })
-              }
-            >
-              -
-            </button>
-          </div>
-        ))}
+              <Label htmlFor={hrid}>{name}</Label>
+              <Input
+                type="number"
+                id={hrid}
+                value={level}
+                onChange={(e) =>
+                  updatePlayerStats({
+                    ...playerStats,
+                    levels: {
+                      ...playerStats.levels,
+                      [hrid]: parseInt(e.target.value, 10),
+                    },
+                  })
+                }
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/services/skills.ts
+++ src/services/skills.ts
@@ -1,5 +1,9 @@
 import { gameData } from "./data";
 
+export const sortedSkills = Object.values(gameData.skillDetailMap).sort(
+  (a, b) => a.sortIndex - b.sortIndex,
+);
+
 export function skillName(skillHrid: string) {
   return gameData.skillDetailMap[skillHrid]?.name ?? skillHrid;
 }
