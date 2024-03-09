21c389cf9567ac3edefc7fe23806dda2c7871fc7
Maxence Frenette <maxence.frenette@gmail.com>
2024-03-09 12:02:45 -0800

    Add state persistence

--- src/services/settings.ts
+++ src/services/settings.ts
@@ -1,21 +1,29 @@
 import { gameData } from "./data";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { z } from "zod";
 
-export interface Settings {
-  levels: Record<string, number>;
-  equipment: Record<string, string | null>;
-  market: {
-    inputBidAskProportion: number;
-    outputBidAskProportion: number;
-    pricePeriod: "latest" | "median";
-  };
-  filters: {
-    hideUnmetLevelRequirements: boolean;
-  };
-}
+const SettingsSchema = z.object({
+  levels: z.record(z.number()),
+  equipment: z.record(z.union([z.string(), z.null()])),
+  market: z.object({
+    inputBidAskProportion: z.number(),
+    outputBidAskProportion: z.number(),
+    pricePeriod: z.union([z.literal("latest"), z.literal("median")]),
+  }),
+  filters: z.object({
+    hideUnmetLevelRequirements: z.boolean(),
+  }),
+});
 
-export interface SettingsState {
-  settings: Settings;
+export type Settings = z.infer<typeof SettingsSchema>;
+
+const PersistedStateSchema = z.object({
+  settings: SettingsSchema,
+});
+type PersistedState = z.infer<typeof PersistedStateSchema>;
+
+export interface SettingsState extends PersistedState {
   updateSettings: (newSettings: Settings) => void;
 }
 
@@ -38,7 +46,73 @@ export const initialSettings: Settings = {
   },
 };
 
-export const useSettingsStore = create<SettingsState>()((set) => ({
-  settings: initialSettings,
-  updateSettings: (newSettings) => set({ settings: newSettings }),
-}));
+function strictMergeRecords<T>(
+  a: Record<string, T>,
+  b: Record<string, T>,
+): Record<string, T> {
+  const result: Record<string, T> = {};
+  for (const key of Object.keys(a)) {
+    if (b[key] === undefined) {
+      result[key] = a[key]!;
+    } else {
+      result[key] = b[key]!;
+    }
+  }
+  return result;
+}
+
+export const useSettingsStore = create<SettingsState>()(
+  persist(
+    (set) => ({
+      settings: initialSettings,
+      updateSettings: (newSettings) => set({ settings: newSettings }),
+    }),
+    {
+      name: "settings",
+      merge: (rawPersistedState, currentState) => {
+        const result =
+          PersistedStateSchema.deepPartial().safeParse(rawPersistedState);
+        if (!result.success) {
+          console.error("Failed to parse persisted settings", result.error);
+          return currentState;
+        }
+
+        const persistedState = result.data;
+        const settings = {
+          levels: strictMergeRecords(
+            currentState.settings.levels,
+            persistedState.settings?.levels ?? {},
+          ),
+          equipment: strictMergeRecords(
+            currentState.settings.equipment,
+            persistedState.settings?.equipment ?? {},
+          ),
+          market: {
+            inputBidAskProportion:
+              persistedState.settings?.market?.inputBidAskProportion ??
+              currentState.settings.market.inputBidAskProportion,
+            outputBidAskProportion:
+              persistedState.settings?.market?.outputBidAskProportion ??
+              currentState.settings.market.outputBidAskProportion,
+            pricePeriod:
+              persistedState.settings?.market?.pricePeriod ??
+              currentState.settings.market.pricePeriod,
+          },
+          filters: {
+            hideUnmetLevelRequirements:
+              persistedState.settings?.filters?.hideUnmetLevelRequirements ??
+              currentState.settings.filters.hideUnmetLevelRequirements,
+          },
+        };
+
+        return {
+          ...currentState,
+          settings,
+        };
+      },
+      partialize: (state) => {
+        return PersistedStateSchema.parse(state);
+      },
+    },
+  ),
+);
